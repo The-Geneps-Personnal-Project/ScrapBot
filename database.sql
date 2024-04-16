0236454b3b4b83842f75a5e46576bb77130aec65
A-DBN <adabin@hotmail.fr>
2024-04-16 16:16:39 +0200

    [+] Add SQLITE Schema

---
+++ database.sql
@@ -0,0 +1,24 @@
+CREATE TABLE mangas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    anilist_id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    chapter TEXT,
+    alert BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE sites (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    site TEXT NOT NULL,
+    url TEXT NOT NULL,
+    chapter_url TEXT NOT NULL,
+    chapter_limiter TEXT NOT NULL,
+    selector TEXT NOT NULL
+);
+
+CREATE TABLE manga_sites (
+    manga_id INTEGER NOT NULL,
+    site_id INTEGER NOT NULL,
+    FOREIGN KEY (manga_id) REFERENCES mangas(id),
+    FOREIGN KEY (site_id) REFERENCES sites(id),
+    PRIMARY KEY (manga_id, site_id)
+);
