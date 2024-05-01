e6e1291b15a798c537343c51ae71bc33896b5ad7
A-DBN <adabin@hotmail.fr>
2024-05-02 00:17:40 +0200

    [+] Added function to get specific element on a page + handle site adding from discord

--- src/seed.ts
+++ src/seed.ts
@@ -1,41 +1,65 @@
 import puppeteer, {Page} from "puppeteer";
+import { SiteInfo } from "./types";
 
-async function getElement(page: Page , selector: string): Promise<Element> {
+export async function getChapterElement(page: Page): Promise<string | null> {
+    const href = await page.evaluate(() => {
+        const links = Array.from(document.querySelectorAll('a'));
+        const targetLink = links.find(link => link.textContent?.toLowerCase().includes("chapter"));
+        return targetLink ? targetLink.href : null;
+    });
+    console.log(href)
+    return href;
+}
+
+export async function getElement(page: Page , selector: string): Promise<string> {
     const link = await page.evaluate((selector: string) => {
         const links = Array.from(document.querySelectorAll(selector));
 
-        let res = []
-
-        for (const link of links) {
-            if (link.querySelector("img")) {
-                res.push(link);
-            }
-        }
-        return res[3]; // 3 To be safe in case the first one are not mangas
+        const filteredLinks = links.filter((link) => {return link.querySelector("img")});
+        return (filteredLinks[3] as HTMLAnchorElement).href;
     }, selector);
 
     return link
 }
 
-export async function addSite(url: string, name: string) {
+export async function addSite(url: string) {
     const browser = await puppeteer.launch({
         headless: true,
         args: ["--no-sandbox"],
-        executablePath: "/usr/bin/chromium-browser",
+        //executablePath: "/usr/bin/chromium-browser", // Remove comment before pushing to prod
     });
 
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: "domcontentloaded" });
 
-    const selector = "a:has(img)"
-    const link = await getElement(page, selector)
+    const link = await getElement(page, "a:has(img)")
+    
+    const site_name = url.split("/")[2]
 
-    console.log(link)
+    await page.goto(link, { waitUntil: "domcontentloaded" });
 
-    const pathSegments = link.getAttribute("href")?.split('/').filter(el => el.length > 0)
+    const pathSegments = link?.split('/').filter(el => el.length > 0)
     pathSegments?.pop()
 
     const list_url = pathSegments?.join("/")
-    console.log(list_url)
+    const chapter_url = await getChapterElement(page)
+
+    // Get -chapter or /chapter depending on the site
+    // for exemple:
+    // https://mangadex.org/manga/exemple-chapter-150
+    // https://mangadex.org/manga/exemple/chapter-150
+    // https://mangadex.org/manga/exemple/chapter/150
+    // TO EDIT IF: Need of the chapter behind or only the character before chapter
+    const chapter_limiter = chapter_url?.split("chapter")[0].split("").pop();
+
+    const site = {
+        site: site_name,
+        url: list_url,
+        chapter_url: chapter_url,
+        chapter_limiter: chapter_limiter,
+    } as SiteInfo
+
+    // TODO: Implement function to add site in database giving site as parameter
+
     await browser.close();
 }
